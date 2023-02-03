c756568d57b7430e5ddd710e848f1d664d0292db
Cris <cristinaghinghiloschi.to@gmail.com>
2023-02-03 19:45:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const decrement = document.querySelector("#decrement");
 const reset = document.querySelector("#reset");
 const increment = document.querySelector("#increment");
 
-increment.addEventListener("click", () => {
-  counter.innerHTML = parseInt(counter.innerHTML) + 1;
+decrement.addEventListener("click", () => {
+  counter.innerHTML = parseInt(counter.innerHTML) - 1;
 });
 
 reset.addEventListener("click", () => {
     counter.innerHTML = 0;
 })
 
-decrement.addEventListener("click", () => {
-  counter.innerHTML = parseInt(counter.innerHTML) - 1;
+increment.addEventListener("click", () => {
+  counter.innerHTML = parseInt(counter.innerHTML) + 1;
 });
