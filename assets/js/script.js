7526dae20be2f28b0926813da4c176a31a7938df
Cris <118299843+CrisGhinghi@users.noreply.github.com>
2023-03-06 22:47:23 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,45 @@
-// seleziono gli elementi HTML
-const counter = document.querySelector("#counter");
-const decrement = document.querySelector("#decrement");
-const reset = document.querySelector("#reset");
-const increment = document.querySelector("#increment");
+// Creo gli elementi
+const container = document.querySelector('#container');
+
+const title = document.createElement('h1');
+title.textContent = `HI, I'M YOUR COUNTER`;
+container.appendChild(title);
+
+const counter = document.createElement('p');
+counter.textContent = '0';
+container.appendChild(counter);
+
+const decrement = document.createElement('button');
+decrement.textContent = '-';
+container.appendChild(decrement);
+
+const reset = document.createElement('button');
+reset.textContent = 'RESET ME';
+container.appendChild(reset);
+
+const increment = document.createElement('button');
+increment.textContent = '+';
+container.appendChild(increment);
 
 
-// aggiungo gli event listener ai bottoni
+// Aggiungo gli event listener ai bottoni
 decrement.addEventListener("click", () => {
-  counter.innerHTML = parseInt(counter.innerHTML) - 1;
+  counter.textContent = parseInt(counter.textContent) - 1;
 });
 
 reset.addEventListener("click", () => {
-    counter.innerHTML = 0;
+    counter.textContent = 0;
 })
 
 increment.addEventListener("click", () => {
-  counter.innerHTML = parseInt(counter.innerHTML) + 1;
+  counter.textContent = parseInt(counter.textContent) + 1;
 });
+
+
+// Aggiungo lo stile
+container.setAttribute('class', 'container');
+title.setAttribute('class', 'title');
+counter.setAttribute('class', 'counter');
+decrement.setAttribute('class', 'button');
+reset.setAttribute('class', 'button');
+increment.setAttribute('class', 'button');
