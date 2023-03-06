2cd1e074cdb4b908930c744e0b340ca9b2077215
Cris <118299843+CrisGhinghi@users.noreply.github.com>
2023-03-06 15:28:20 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+// seleziono gli elementi HTML
 const counter = document.querySelector("#counter");
 const decrement = document.querySelector("#decrement");
 const reset = document.querySelector("#reset");
 const increment = document.querySelector("#increment");
 
+
+// aggiungo gli event listener ai bottoni
 decrement.addEventListener("click", () => {
   counter.innerHTML = parseInt(counter.innerHTML) - 1;
 });
